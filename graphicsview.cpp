cf7968d066149cc3325d8eca87e8953225b689c6
zasovin <zasovin@raa-st.com>
2019-08-12 16:15:21 +0300

    Доделал для выделения и ступенек

--- graphicsview.cpp
+++ graphicsview.cpp
@@ -695,7 +695,35 @@ void	GraphicsView::drawGraphArea(const vec2& areaBL, const vec2& areaSize)
 			for(size_t j = 0; j < m_SelectedObjects.size(); j++)
 				if(m_SelectedObjects.at(j) == pAxe)
 					continue;
+			
+			//Рисуем в отдельную текстуру
+			glBindFramebuffer(GL_FRAMEBUFFER, fboGraph);
+			glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			glBlendFunc(GL_ONE, GL_ZERO);
+			glBlendEquationSeparate(GL_FUNC_ADD, GL_MAX);
+
 			pAxe->draw(Time0, TimeScale, gridStep, areaBL, areaSize, 0.3f);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			glBlendEquation(GL_FUNC_ADD);
+			//QImage img = qt_gl_read_framebuffer(size() * devicePixelRatio(), false, false);
+			//img.setDevicePixelRatio(devicePixelRatio());
+			//img.save("c:\\0.png");
+
+			//Переносим ее в основную
+			glBindFramebuffer(GL_FRAMEBUFFER, fboGraphArea);
+			m_fbo_program->bind();
+			glEnable(GL_BLEND);
+			glBindBuffer(GL_ARRAY_BUFFER, pageVBO);
+			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)0);
+			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(2*sizeof(float)));
+			glEnableVertexAttribArray(1);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, fboGraphTexture);
+			glDrawArrays(GL_TRIANGLE_STRIP, 24, 4);
+			glBindTexture(GL_TEXTURE_2D, 0);
+			m_fbo_program->release();
 		}
 		else
 		{
@@ -735,27 +763,37 @@ void	GraphicsView::drawGraphArea(const vec2& areaBL, const vec2& areaSize)
 	{
 		Graph::GraphObject*	pGraph	= m_SelectedObjects.at(j);
 		if(pGraph->m_Type == AXE)
+		{
+			//Рисуем в отдельную текстуру
+			glBindFramebuffer(GL_FRAMEBUFFER, fboGraph);
+			glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			glBlendFunc(GL_ONE, GL_ZERO);
+			glBlendEquationSeparate(GL_FUNC_ADD, GL_MAX);
+
 			pGraph->draw(Time0, TimeScale, gridStep, areaBL, areaSize, 1.0f);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			glBlendEquation(GL_FUNC_ADD);
+			//QImage img = qt_gl_read_framebuffer(size() * devicePixelRatio(), false, false);
+			//img.setDevicePixelRatio(devicePixelRatio());
+			//img.save("c:\\0.png");
+
+			//Переносим ее в основную
+			glBindFramebuffer(GL_FRAMEBUFFER, fboGraphArea);
+			m_fbo_program->bind();
+			glEnable(GL_BLEND);
+			glBindBuffer(GL_ARRAY_BUFFER, pageVBO);
+			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)0);
+			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(2*sizeof(float)));
+			glEnableVertexAttribArray(1);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, fboGraphTexture);
+			glDrawArrays(GL_TRIANGLE_STRIP, 24, 4);
+			glBindTexture(GL_TEXTURE_2D, 0);
+			m_fbo_program->release();
+		}
 	}
-
-	//QImage img = qt_gl_read_framebuffer(size() * devicePixelRatio(), false, false);
-	//img.setDevicePixelRatio(devicePixelRatio());
-	//img.save("c:\\0.png");
-
-	////Копируем на экран из текстуры
-	//glDisable(GL_BLEND);
-	//m_fbo_program->bind();
-	//glBindBuffer(GL_ARRAY_BUFFER, pageVBO);
-	//glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)0);
-	//glEnableVertexAttribArray(0);
-	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(2*sizeof(float)));
-	//glEnableVertexAttribArray(1);
-	//glActiveTexture(GL_TEXTURE0);
-	//glBindTexture(GL_TEXTURE_2D, fboGraphAreaTexture);
-	//glDrawArrays(GL_TRIANGLE_STRIP, 20, 4);
-	//glBindTexture(GL_TEXTURE_2D, 0);
-	//m_fbo_program->release();
-	//glEnable(GL_BLEND);
 
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	fboGraphAreaValid	= true;
